c791b7573dab271921643660a263571db883a198
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-06-17 15:57:59 +0100

    Implement tmux_navigate

--- lua/tmux-nvim/init.lua
+++ lua/tmux-nvim/init.lua
@@ -1,7 +1,18 @@
+local Tmux = require("tmux-nvim/tmux")
 local Plugin = {}
 
+-- Given a direction key (h, j, k, l), attempt to navigate within vim.
+-- If the vim focus has not changed, relay the navigation command to tmux.
 function Plugin.tmux_navigate(direction)
-  --
+  starting_window = vim.fn.winnr()
+
+  vim.cmd.wincmd(direction)
+
+  ending_window = vim.fn.winnr()
+
+  if Tmux.is_active() and starting_window == ending_window then
+    Tmux.navigate(direction)
+  end
 end
 
 return Plugin
